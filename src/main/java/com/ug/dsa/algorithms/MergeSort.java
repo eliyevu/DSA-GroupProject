90338fa66815990015473403bc6f4b3464717a36
SukahJerryEli <jesukah@st.ug.edu.gh>
2026-08-10 21:11:44 +0000

    Add files via upload

---
+++ src/main/java/com/ug/dsa/algorithms/MergeSort.java
@@ -0,0 +1,54 @@
+package com.ug.dsa.algorithms;
+
+public class MergeSort {
+
+    public static void sort(int[] arr) {
+        if (arr.length < 2) {
+            return;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        for (int i = 0; i < mid; i++) {
+            left[i] = arr[i];
+        }
+        for (int i = mid; i < arr.length; i++) {
+            right[i - mid] = arr[i];
+        }
+
+        sort(left);
+        sort(right);
+        merge(arr, left, right);
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < left.length) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < right.length) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+}
