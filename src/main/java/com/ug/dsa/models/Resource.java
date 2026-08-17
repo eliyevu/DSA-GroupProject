81961106ab84ae061de6aa57c01b76d82ea15a07
owner <owner@owners-MacBook-Pro.local>
2026-08-17 03:01:55 +0000

    Implement IndexingService findLocation/findResource using BTree; add minimal id/name fields to Location and Resource models

--- src/main/java/com/ug/dsa/models/Resource.java
+++ src/main/java/com/ug/dsa/models/Resource.java
@@ -1,4 +1,36 @@
 package com.ug.dsa.models;
 
+/**
+ * Minimal Resource model.
+ *
+ * NOTE: this was an empty stub. Added the minimum fields needed to
+ * support Team 4's Indexing/Search service (findResource by id).
+ * Whoever owns this model, feel free to extend with more fields
+ * (type, capacity, etc.) - just keep the id field since
+ * IndexingService depends on it.
+ *
+ * Added by: Amoah Edward Junior (Member 7)
+ */
 public class Resource {
+
+    private String id;
+    private String name;
+
+    public Resource(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Resource{id='" + id + "', name='" + name + "'}";
+    }
 }
